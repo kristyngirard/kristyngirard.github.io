350765c3953c95a2271f0790ba1dcbf31da47567
Kristyn Girard <kristyngirard@cmail.carleton.ca>
2015-02-07 21:15:59 -0500

    Layout updates
    Added animated name, formatted elements on the page

---
+++ main.js
@@ -0,0 +1,22 @@
+var myName = "Kristyn Girard";
+
+var red = [0, 100, 50];
+var orange = [40, 100, 60];
+var green = [75, 100, 40];
+var blue = [196, 77, 55];
+var purple = [320, 80, 45];
+var pink = [50, 100, 100];
+var letterColors = [purple];
+
+drawName(myName, letterColors);
+
+if(10 < 3)
+{
+    bubbleShape = 'square';
+}
+else
+{
+    bubbleShape = 'circle';
+}
+
+bounceBubbles();
